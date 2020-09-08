1fb733763ee47eef1a3e1cc24d115c2f042d2bc2
Nish Tahir <nishtahir@outlook.com>
2020-09-08 11:42:53 -0400

    Add regression test for nested comments

---
+++ test/regression/20.test.kt
@@ -0,0 +1,18 @@
+# SYNTAX TEST "source.kotlin" "Comments"
+
+ /*
+#^^ source.kotlin comment.block.kotlin punctuation.definition.comment.begin.kotlin
+      This is inside the comment
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ source.kotlin comment.block.kotlin
+     /*
+#^^^^ source.kotlin comment.block.kotlin
+#    ^^ source.kotlin comment.block.kotlin
+         You can insert something here.
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ source.kotlin comment.block.kotlin
+     */
+#^^^^ source.kotlin comment.block.kotlin
+#    ^^ source.kotlin comment.block.kotlin
+      This is a comment since the nested comment is parsed correctly.
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ source.kotlin comment.block.kotlin
+ */
+#^^ source.kotlin comment.block.kotlin punctuation.definition.comment.end.kotlin

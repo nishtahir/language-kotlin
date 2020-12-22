bf79efd547867b2a9302445bab4fefecf645266f
Sascha Häberling <mail@sascha-haeberling.com>
2020-12-21 19:31:03 -0800

    Addresses double-quote in character issue from issue #26.
    Uses a negative lookahead regex to ensure that no single quote is
    allowed on either side of the double-quote.
    
    Verified using Lightshow on the following source:
    https://github.com/shaeberling/euler/blob/master/kotlin/src/com/s13g/aoc/aoc2020/Day19.kt
    
    Also added a regression test case that will fail without this change.

---
+++ test/regression/26.test.kt
@@ -0,0 +1,6 @@
+// SYNTAX TEST "source.kotlin" "Double quotes in character value."
+  foo('"')
+//^^^ source.kotlin
+//   ^ source.kotlin meta.group.kotlin punctuation.section.group.begin.kotlin
+//    ^^^ source.kotlin meta.group.kotlin
+//       ^ source.kotlin meta.group.kotlin punctuation.section.group.end.kotlin

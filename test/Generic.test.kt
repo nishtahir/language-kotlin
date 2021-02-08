e968288236401d322c8d16a96540bc0dc141f920
Animesh Sahu <animeshsahu19@yahoo.com>
2021-02-08 10:39:22 +0530

    Match generics even in deep nested contexts. Match inherited contexts correctly (#41)
    Fixes #19 and #40.

---
+++ test/Generic.test.kt
@@ -0,0 +1,28 @@
+// SYNTAX TEST "source.kotlin" "Generic"
+class Test<in T : CharSequence> {
+//        ^ punctuation.bracket.angle.begin.kotlin
+//         ^^ storage.modifier.generic.variance.kotlin
+//            ^ meta.class.kotlin entity.name.type.class.kotlin
+//                ^^^^^^^^^^^^ meta.class.kotlin support.class.kotlin
+//                            ^ punctuation.bracket.angle.end.kotlin
+    fun <E : Number> do(input: T) {
+//      ^ punctuation.bracket.angle.begin.kotlin
+//       ^ meta.class.kotlin entity.name.type.class.kotlin
+//         ^ keyword.operator.declaration.kotlin
+//           ^^^^^^ meta.class.kotlin entity.name.type.class.kotlin
+//                 ^ punctuation.bracket.angle.end.kotlin
+    }
+}
+
+val list: MutableList<*>
+//                   ^ punctuation.bracket.angle.begin.kotlin
+//                    ^ entity.name.type.generic.wildcard.kotlin
+//                     ^ punctuation.bracket.angle.end.kotlin
+
+fun <T, E> test() {
+//  ^ punctuation.bracket.angle.begin.kotlin
+//   ^ meta.function.kotlin entity.name.type.class.kotlin
+//    ^ punctuation.seperator.kotlin
+//      ^ meta.function.kotlin entity.name.type.class.kotlin
+//       ^ punctuation.bracket.angle.end.kotlin
+}

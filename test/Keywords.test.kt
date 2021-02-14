62ede474af1dc1e56cf4283bb326594dc13b3cd8
Animesh Sahu <animeshsahu19@yahoo.com>
2021-02-14 13:23:49 +0530

    Fix #44

--- test/Keywords.test.kt
+++ test/Keywords.test.kt
@@ -70,3 +70,17 @@
 //        ^ source.kotlin meta.block.kotlin punctuation.section.group.begin.kotlin
   }
 //^ source.kotlin meta.block.kotlin punctuation.section.group.end.kotlin
+
+  val test = !isC
+//^^^ source.kotlin storage.type.kotlin
+//   ^^^^^^ source.kotlin
+//         ^ source.kotlin keyword.operator.assignment.kotlin
+//           ^ keyword.operator.logical.kotlin
+//            ^^^ source.kotlin - keyword.operator.kotlin
+
+  val test2 =!isC
+//          ^ keyword.operator.assignment.kotlin
+//           ^^^ - keyword.operator.kotlin
+
+if (test3!is String) return
+//       ^^^ source.kotlin meta.group.kotlin keyword.operator.kotlin

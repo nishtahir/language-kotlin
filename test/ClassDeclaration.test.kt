85dcec13b135f932988ecceef8b14e367af89c48
Animesh Sahu <animeshsahu19@yahoo.com>
2021-05-13 11:51:59 +0530

    Closes #48, Closes #50; Rename inline to value, allow soft-keywords to be used as names, recognize functional interfaces, add regressions and tests.

--- test/ClassDeclaration.test.kt
+++ test/ClassDeclaration.test.kt
@@ -36,3 +36,22 @@ actual class Foo(bar: String) {
 }
 // <--- ^ punctuation.section.group.begin.kotlin
 
+fun interface ITestInterface {
+// <------------- meta.class.kotlin storage.modifier.kotlin
+//            ^^^^^^^^^^^^^^ source.kotlin meta.class.kotlin entity.name.class.kotlin
+//                           ^ meta.block.kotlin punctuation.section.group.begin.kotlin
+    fun sam()
+//  ^^^ keyword.other.kotlin
+//      ^^^ entity.name.function.kotlin
+//         ^ meta.function.kotlin meta.parameters.kotlin punctuation.section.group.begin.kotlin punctuation.definition.parameters.begin.kotlin
+//          ^ meta.function.kotlin meta.parameters.kotlin punctuation.section.group.end.kotlin punctuation.definition.parameters.end.kotlin
+}
+// <- source.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.end.kotlin
+
+value class UInt(private val value: Int) {}
+// <----------- meta.class.kotlin storage.modifier.kotlin
+//          ^^^^ source.kotlin meta.class.kotlin entity.name.class.kotlin
+//              ^ meta.class.kotlin meta.parameters.kotlin punctuation.section.group.begin.kotlin punctuation.definition.parameters.begin.kotlin
+//                           ^^^ variable.parameter.function.kotlin
+//                                  ^^^ support.class.kotlin
+//                                     ^ meta.class.kotlin meta.parameters.kotlin punctuation.section.group.end.kotlin punctuation.definition.parameters.end.kotlin

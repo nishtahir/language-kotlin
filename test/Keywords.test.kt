a2e69723f2b61171e415edbc2d0ed0228456add2
Animesh Sahu <animeshsahu19@yahoo.com>
2021-07-14 19:58:33 +0530

    Rename inline to value, allow soft-keywords to be used as names, recognize functional interfaces (#55)
    * Closes #48, Closes #50; Rename inline to value, allow soft-keywords to be used as names, recognize functional interfaces, add regressions and tests.
    
    * Adds inline keyword back as it still exists for functions if not classes, move const and lateinit to soft keyword category, add value interface as well to valid keyword in declaration of interfaces and add tests for value & copy

--- test/Keywords.test.kt
+++ test/Keywords.test.kt
@@ -70,3 +70,37 @@
 //        ^ source.kotlin meta.block.kotlin punctuation.section.group.begin.kotlin
   }
 //^ source.kotlin meta.block.kotlin punctuation.section.group.end.kotlin
+
+// Taken from: https://github.com/Kotlin/KEEP/blob/master/notes/value-classes.md#value-interfaces
+value interface ImmutableList<T> {
+// <--------------- source.kotlin meta.class.kotlin storage.modifier.kotlin
+//              ^^^^^^^^^^^^^ source.kotlin meta.class.kotlin entity.name.class.kotlin
+//                           ^ source.kotlin meta.class.kotlin punctuation.bracket.angle.begin.kotlin
+//                            ^ source.kotlin meta.class.kotlin entity.name.type.class.kotlin
+//                             ^ source.kotlin meta.class.kotlin punctuation.bracket.angle.end.kotlin
+//                               ^ source.kotlin meta.class.kotlin punctuation.section.group.begin.kotlin
+    copy fun add(element: T)
+//  ^^^^ source.kotlin meta.class.kotlin storage.type.kotlin
+//       ^^^ source.kotlin meta.class.kotlin meta.function.kotlin keyword.other.kotlin
+//           ^^^ source.kotlin meta.class.kotlin meta.function.kotlin entity.name.function.kotlin
+//              ^ source.kotlin meta.class.kotlin meta.function.kotlin punctuation.section.group.begin.kotlin punctuation.definition.parameters.begin.kotlin
+//               ^^^^^^^ source.kotlin meta.class.kotlin meta.function.kotlin variable.parameter.function.kotlin
+//                      ^ source.kotlin meta.class.kotlin meta.function.kotlin keyword.operator.declaration.kotlin
+//                        ^ source.kotlin meta.class.kotlin meta.function.kotlin
+//                         ^ source.kotlin meta.class.kotlin meta.function.kotlin punctuation.section.group.end.kotlin punctuation.definition.parameters.end.kotlin
+}
+// <- source.kotlin meta.class.kotlin punctuation.section.group.end.kotlin
+
+value class State(
+// <----------- source.kotlin meta.class.kotlin storage.modifier.kotlin
+//          ^^^^^ source.kotlin meta.class.kotlin entity.name.class.kotlin
+//               ^ source.kotlin meta.class.kotlin punctuation.section.group.begin.kotlin punctuation.definition.parameters.begin.kotlin
+    copy var lastUpdate: Instant,
+//  ^^^^ source.kotlin meta.class.kotlin meta.parameters.kotlin storage.type.kotlin
+//       ^^^ source.kotlin meta.class.kotlin meta.parameters.kotlin storage.type.kotlin
+//           ^^^^^^^^^^ source.kotlin meta.class.kotlin meta.parameters.kotlin variable.parameter.function.kotlin
+//                     ^ source.kotlin meta.class.kotlin meta.parameters.kotlin  keyword.operator.declaration.kotlin
+//                       ^^^^^^^ source.kotlin meta.class.kotlin meta.parameters.kotlin  entity.name.type.class.kotlin
+//                              ^ source.kotlin meta.class.kotlin meta.parameters.kotlin punctuation.seperator.kotlin
+)
+// <- source.kotlin meta.class.kotlin punctuation.section.group.end.kotlin punctuation.definition.parameters.end.kotlin

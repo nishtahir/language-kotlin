dec06c8e7b6dbe76aef95664f2ec891ef8ac06eb
Nish Tahir <nishtahir@outlook.com>
2022-11-30 09:47:09 -0500

    Correct scopes syntax

--- test/regression/63.test.kt
+++ test/regression/63.test.kt
@@ -1,20 +1,20 @@
-// SYNTAX TEST "source.kotlin" "Keywords"
+// SYNTAX TEST "source.kotlin" "Recognize inner modifier"
 
-class Outer {
-//^^^^^ source.kotlin meta.class.kotlin storage.modifier.kotlin
-//     ^ source.kotlin meta.class.kotlin
-//      ^^^^^ source.kotlin meta.class.kotlin entity.name.class.kotlin
-//           ^ source.kotlin meta.class.kotlin
-//            ^ source.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.begin.kotlin
-    inner class Inner {}
-//  ^^^^ source.kotlin meta.class.kotlin meta.block.kotlin
-//      ^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin storage.modifier.kotlin
-//           ^ source.kotlin meta.class.kotlin meta.block.kotlin
-//            ^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin storage.modifier.kotlin
-//                 ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
-//                  ^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin entity.name.class.kotlin
-//                       ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
-//                        ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.begin.kotlin
-//                         ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.end.kotlin
-}
-//^ source.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.end.kotlin
+>class Outer {
+#^^^^^ source.kotlin meta.class.kotlin storage.modifier.kotlin
+#     ^ source.kotlin meta.class.kotlin
+#      ^^^^^ source.kotlin meta.class.kotlin entity.name.class.kotlin
+#           ^ source.kotlin meta.class.kotlin
+#            ^ source.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.begin.kotlin
+>    inner class Inner {}
+#^^^^ source.kotlin meta.class.kotlin meta.block.kotlin
+#    ^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin storage.modifier.kotlin
+#         ^ source.kotlin meta.class.kotlin meta.block.kotlin
+#          ^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin storage.modifier.kotlin
+#               ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
+#                ^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin entity.name.class.kotlin
+#                     ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
+#                      ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.begin.kotlin
+#                       ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.end.kotlin
+>}
+#^ source.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.end.kotlin

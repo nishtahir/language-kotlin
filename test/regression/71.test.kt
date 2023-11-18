b65a18d4c23eedcd2bc280cd04282d29d191721a
Nish Tahir <nishtahir@outlook.com>
2023-11-18 11:29:36 -0500

    Add support for Data objects (#75)
    * add support for data object (#71)
    
    * Add update to corpus and regression test
    
    ---------
    
    Co-authored-by: Jan Škrášek <jan@skrasek.com>

---
+++ test/regression/71.test.kt
@@ -0,0 +1,19 @@
+// SYNTAX TEST "source.kotlin" "Add Data Object"
+
+>    data object Error : ProfileScreenState
+#^^^^ source.kotlin meta.class.kotlin meta.block.kotlin
+#    ^^^^^^^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin storage.modifier.kotlin
+#               ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
+#                ^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin entity.name.class.kotlin
+#                     ^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
+#                        ^^^^^^^^^^^^^^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin entity.name.type.class.kotlin
+>    data object Loading : ProfileScreenState
+#^^^^ source.kotlin meta.class.kotlin meta.block.kotlin
+#    ^^^^^^^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin storage.modifier.kotlin
+#               ^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
+#                ^^^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin entity.name.class.kotlin
+#                       ^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin
+#                          ^^^^^^^^^^^^^^^^^^ source.kotlin meta.class.kotlin meta.block.kotlin meta.class.kotlin entity.name.type.class.kotlin
+>}
+#^ source.kotlin meta.class.kotlin meta.block.kotlin punctuation.section.group.end.kotlin
+>

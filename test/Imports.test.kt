4886027e5ed8c661bb66d711dec25788136a6bdc
Ksenia Korovina <ks.korovina@yandex.ru>
2024-04-28 15:07:10 +0200

    Fix detection of imports with trailing comments

--- test/Imports.test.kt
+++ test/Imports.test.kt
@@ -32,3 +32,20 @@
 //                                          ^^ source.kotlin meta.import.kotlin keyword.other.kotlin
 //                                            ^ source.kotlin meta.import.kotlin
 //                                             ^^^^ source.kotlin meta.import.kotlin entity.name.type
+
+  import com.google.test.CommentedClass // comment
+//^^^^^^ source.kotlin meta.import.kotlin keyword.other.import.kotlin
+//      ^ source.kotlin meta.import.kotlin
+//       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ source.kotlin meta.import.kotlin storage.modifier.import.kotlin
+//                                      ^^ source.kotlin punctuation.definition.comment.kotlin comment.line.double-slash.kotlin
+
+  import com.google.test.CommentedClass /*comment*/ as Another
+//^^^^^^ source.kotlin meta.import.kotlin keyword.other.import.kotlin
+//      ^ source.kotlin meta.import.kotlin
+//       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ source.kotlin meta.import.kotlin storage.modifier.import.kotlin
+//                                      ^^ source.kotlin comment.block.kotlin punctuation.definition.comment.begin.kotlin
+//                                               ^^ source.kotlin comment.block.kotlin punctuation.definition.comment.end.kotlin
+//                                                 ^ source.kotlin
+//                                                  ^^ source.kotlin keyword.operator.kotlin
+//                                                    ^ source.kotlin
+//                                                     ^^^^^^^ source.kotlin entity.name.type.class.kotlin
